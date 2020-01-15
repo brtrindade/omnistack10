4c7551e26ac1b726cfc7a62f58116aef0b378e9d
brtrindade <bruno-miguel@live.com>
2020-01-15 15:48:51 -0300

    add cors ao backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-rkzme.mongodb.net/w
   useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
