23bb683ede22551d5afddd1881e9437fc4cf40e8
brtrindade <bruno-miguel@live.com>
2020-01-15 21:56:13 -0300

    add feature delete

--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../../services/api';
 
 import './styles.css';
 
 function DevItem({dev}) {
+  const [show, setShow] = useState(false)
+
+  async function handleExclude(dev) {
+    await api.delete(`/devs/${dev.github_username}`);
+  }
+
   return(
-    <li className="dev-item">
+    <li className="dev-item" onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
       <header>
         <img src={dev.avatar_url} alt={dev.name}/>
         <div className="user-info">
           <strong>{dev.name}</strong>
           <span>{dev.techs.join(', ')}</span>
         </div>
+        {show && (<button className="exclude" onClick={() => handleExclude(dev)}>X</button>) }
       </header>
       <p>{dev.bio}</p>
       <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
